7be0b4c0168301cf67db402911afbd9d971cb274
Luis Vieira <lvieira@lvieira.com>
2025-04-17 14:50:47 -0700

    fixing shutdown

--- examples/app.py
+++ examples/app.py
@@ -9,14 +9,14 @@ if __name__ == "__main__":
         config=EventideConfig(
             handler_paths={"./examples"},
             queue=MockQueueConfig(
-                buffer_size=1000,
+                buffer_size=20,
                 min_messages=1,
-                max_messages=100,
+                max_messages=10,
             ),
-            concurrency=20,
-            timeout=3.0,
+            concurrency=2,
+            timeout=2.0,
             retry_for=[TimeoutError],
-            retry_limit=3,
+            retry_limit=2,
         ),
     )
     app.run()
